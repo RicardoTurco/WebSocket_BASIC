4476f4d08089a8461d79dde053e840a8ed9e633b
RicardoTurco <ricturco@gmail.com>
2019-04-04 16:15:07 -0300

    refactor in app.py

--- WebSocket_SERVER/app.py
+++ WebSocket_SERVER/app.py
@@ -3,12 +3,9 @@ from flask import Flask
 import asyncio
 import websockets
 
+
 app = Flask(__name__)
 
-
-# @app.route("/")
-# def index():
-#     return "Hello World"
 
 @asyncio.coroutine
 def hello(websocket, path):
@@ -24,6 +21,7 @@ start_server = websockets.serve(hello, 'localhost', 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
+
 
 if __name__ == "__main__":
     app.run()
