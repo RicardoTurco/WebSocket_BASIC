c2a09fc807804ef251ba86d42340fcfe0fde2581
RicardoTurco <ricturco@gmail.com>
2019-04-11 16:20:27 -0300

    add TESTING_DICT method

--- WebSocket_CLIENT/app.py
+++ WebSocket_CLIENT/app.py
@@ -3,6 +3,8 @@ from flask import Flask
 import asyncio
 import websockets
 
+import json
+
 
 app = Flask(__name__)
 
@@ -50,8 +52,40 @@ def soma():
         a = ''
 
 
+@asyncio.coroutine
+def testing_dict():
+    websocket = yield from websockets.connect(
+        'ws://localhost:8765/')
+
+    try:
+        je = json.dumps({'painel_id': '1', 'transmission_status_value': False})
+        jef = json.loads(je)
+
+        yield from websocket.send(je)
+        print()
+        print("< o JSON sent is: {}".format(je))
+        print("< o painel_id sent is: {}".format(jef['painel_id']))
+        print("< o transmission_status_value sent is: {}".format(jef['transmission_status_value']))
+        print()
+
+        jr = yield from websocket.recv()
+        jrf = json.loads(jr)
+
+        jro = json.dumps({'painel_id': jrf['painel_id'], 'transmission_status_value': jrf['transmission_status_value']}, sort_keys=True)
+
+        print()
+        print("> o JSON receiving is: {}".format(jr))
+        print("> o painel_id receiving is: {}".format(jrf['painel_id']))
+        print("> o transmission_status_value receiving is: {}".format(jrf['transmission_status_value']))
+        print()
+
+    finally:
+        yield from websocket.close()
+
+
 # asyncio.get_event_loop().run_until_complete(hello())
-asyncio.get_event_loop().run_until_complete(soma())
+# asyncio.get_event_loop().run_until_complete(soma())
+asyncio.get_event_loop().run_until_complete(testing_dict())
 
 
 if __name__ == "__main__":
--- WebSocket_SERVER/app.py
+++ WebSocket_SERVER/app.py
@@ -38,8 +38,32 @@ def soma(websocket, path):
     print("< the result sent was: {}".format(d))
 
 
+@asyncio.coroutine
+def testing_dict(websocket, path):
+    jr = yield from websocket.recv()
+    jrf = json.loads(jr)
+
+    jro = json.dumps({'painel_id': jrf['painel_id'], 'transmission_status_value': jrf['transmission_status_value']}, sort_keys=True)
+
+    print()
+    print("> o JSON received was: {}".format(jro))
+    print("> o painel_id received: {}".format(jrf['painel_id']))
+    print("> o transmission_status_value received: {}".format(jrf['transmission_status_value']))
+    print()
+
+    je = json.dumps({'painel_id': '3', 'transmission_status_value': True}, sort_keys=True)
+    jef = json.loads(je)
+
+    yield from websocket.send(je)
+    print("< o JSON sent was: {}".format(je))
+    print("< o painel_id sent was: {}".format(jef['painel_id']))
+    print("< o transmission_status_value sent was: {}".format(jef['transmission_status_value']))
+    print()
+
+
 # start_server = websockets.serve(hello, 'localhost', 8765)
-start_server = websockets.serve(soma, 'localhost', 8765)
+# start_server = websockets.serve(soma, 'localhost', 8765)
+start_server = websockets.serve(testing_dict, 'localhost', 8765)
 
 
 asyncio.get_event_loop().run_until_complete(start_server)
