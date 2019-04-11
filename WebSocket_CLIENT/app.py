ba7315dfccc6ab2741381b6ab2b4375afc660390
RicardoTurco <ricturco@gmail.com>
2019-04-11 15:34:23 -0300

    add SOMA method

--- WebSocket_CLIENT/app.py
+++ WebSocket_CLIENT/app.py
@@ -24,7 +24,34 @@ def hello():
     finally:
         yield from websocket.close()
 
-asyncio.get_event_loop().run_until_complete(hello())
+
+@asyncio.coroutine
+def soma():
+    websocket = yield from websockets.connect(
+        'ws://localhost:8765/')
+
+    try:
+        ne = "2,3"
+        nef = list(ne.split(","))
+
+        yield from websocket.send(ne)
+        print()
+        print("< the first number sent is: {}".format(nef[0]))
+        print("< the second number sent is: {}".format(nef[1]))
+        print()
+
+        nr = yield from websocket.recv()
+        print()
+        print("> the result received is: {}".format(str(nr)))
+        print()
+
+    finally:
+        yield from websocket.close()
+        a = ''
+
+
+# asyncio.get_event_loop().run_until_complete(hello())
+asyncio.get_event_loop().run_until_complete(soma())
 
 
 if __name__ == "__main__":
--- WebSocket_SERVER/app.py
+++ WebSocket_SERVER/app.py
@@ -3,6 +3,8 @@ from flask import Flask
 import asyncio
 import websockets
 
+import json
+
 
 app = Flask(__name__)
 
@@ -17,7 +19,28 @@ def hello(websocket, path):
     yield from websocket.send(greeting)
     print("< {}".format(greeting))
 
-start_server = websockets.serve(hello, 'localhost', 8765)
+
+@asyncio.coroutine
+def soma(websocket, path):
+    nr = yield from websocket.recv()
+
+    nrf = list(nr.split(","))
+
+    print()
+    print("> the first number received was: {}".format(nrf[0]))
+    print("> the second number received was: {}".format(nrf[1]))
+    print()
+
+    c = (int(nrf[0]) + int(nrf[1]))
+    d = str(c)
+
+    yield from websocket.send(d)
+    print("< the result sent was: {}".format(d))
+
+
+# start_server = websockets.serve(hello, 'localhost', 8765)
+start_server = websockets.serve(soma, 'localhost', 8765)
+
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
